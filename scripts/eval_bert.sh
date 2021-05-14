69b0102ee3e32445ec8be70f232cc56c42a2bb95
Mayank Anand <36782063+mayankanand007@users.noreply.github.com>
2021-05-14 16:51:58 +0530

    Update eval_bert.sh

--- scripts/eval_bert.sh
+++ scripts/eval_bert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 export CUDA_VISIBLE_DEVICES=0 
 
-PATH_TO_DATA=/h/xinji/projects/GLUE
+PATH_TO_DATA=${1}
 
 MODEL_TYPE=bert  # bert or roberta
 MODEL_SIZE=base  # base or large
@@ -14,14 +14,14 @@ then
 fi
 
 
-python -um examples.run_highway_glue \
+python -um examples.run_glue \
   --model_type $MODEL_TYPE \
-  --model_name_or_path ./saved_models/${MODEL_TYPE}-${MODEL_SIZE}/$DATASET/two_stage \
+  --model_name_or_path ./saved_models/${MODEL_TYPE}-${MODEL_SIZE}/$DATASET/raw \
   --task_name $DATASET \
   --do_eval \
   --do_lower_case \
   --data_dir $PATH_TO_DATA/$DATASET \
-  --output_dir ./saved_models/${MODEL_TYPE}-${MODEL_SIZE}/$DATASET/two_stage \
+  --output_dir ./saved_models/${MODEL_TYPE}-${MODEL_SIZE}/$DATASET/raw \
   --plot_data_dir ./plotting/ \
   --max_seq_length 128 \
   --eval_each_highway \
